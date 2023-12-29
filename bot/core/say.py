17c47df2e6c5406ac3c106a13ff80b426b8cd31f
Kona,_,Kust <conacust@gmail.com>
2023-12-29 13:57:01 +0700

    feat: Make "about bot" working

--- bot/core/say.py
+++ bot/core/say.py
@@ -1,1 +1,20 @@
-about_self = "Я бот, созданный Конатуром Кустом"
+from random import choice
+
+self_text = [
+    "Я бот, созданный Конатуром Кустом",
+    "Unkcat помогал меня делать \(:",
+    "Мне нравится бумага",
+    "Мой любимый цвет \- прозрачный\!",
+]
+
+selected = []
+
+
+async def about_self():
+    text = choice(self_text)
+    while text in selected:
+        text = choice(self_text)
+    selected.append(text)
+    if len(selected) == 3:
+        del selected[0]
+    return text

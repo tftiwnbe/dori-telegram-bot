a00c92eef56bb9579a1fe55e2f6a78e7eb803a88
tftiwnbe <wnbe@hmphin.space>
2024-01-01 02:16:57 +0700

    feat: Add workable "help" method to feedback

--- bot/admin/loader.py
+++ bot/admin/loader.py
@@ -1,8 +1,13 @@
 from loguru import logger
-from bot.admin.handlers import admin_panel, user_management, send_as_bot
+from bot.admin.handlers import admin_panel, user_management, send_as_bot, help_to_users
 from runners.launch import dp_main as dp
 
 
 def enable() -> None:  # Регестрация Роутеров
-    dp.include_routers(admin_panel.router, user_management.router, send_as_bot.router)
+    dp.include_routers(
+        admin_panel.router,
+        user_management.router,
+        send_as_bot.router,
+        help_to_users.router,
+    )
     logger.info("Admin routers included")
--- bot/core/keyboards/main_menu.py
+++ bot/core/keyboards/main_menu.py
@@ -10,8 +10,16 @@ menu_kb = [
         IButton(text="Опоздания", callback_data="bad_humans"),
     ],
     [
-        IButton(text="Помощь", callback_data="help"),
+        IButton(text="Помощь", callback_data="help_menu"),
+    ],
+]
+
+help_kb = [
+    [
+        IButton(text="<--Главное меню", callback_data="main_menu"),
+        IButton(text="Связь с богами", callback_data="say_to_admin"),
     ],
 ]
 
 menu_kb = IMarkup(inline_keyboard=menu_kb)
+help_kb = IMarkup(inline_keyboard=help_kb)

1b8a5731ef025c57943489eff3ae3439bb5f27b6
Kona,_,Kust <conacust@gmail.com>
2024-01-15 16:23:29 +0700

    fix: No more errors on shutdown

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-import asyncio
 # import logging
 # import sys
+import asyncio
 
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiohttp import web
 from loguru import logger
 
-from config import loguru
-from config.config_reader import config
-from runners.launch import bot
-from runners.launch import dp_main as dp
-
-HOST = config.web_server_host.get_secret_value()
-PORT = int(config.web_server_port.get_secret_value())
-URL = config.base_webhook_url.get_secret_value()
-PATH = "/webhook"
-
 loop = asyncio.get_event_loop()  # Ссылка на текущий цикл событий
-logger.info("main.py was imported")
+
+
+def read_configs():
+    from config.config_reader import config
+
+    global HOST
+    global PORT
+    global URL
+    global PATH
+
+    HOST = config.web_server_host.get_secret_value()
+    PORT = int(config.web_server_port.get_secret_value())
+    URL = config.base_webhook_url.get_secret_value()
+    PATH = "/aiogram"
 
 
 async def on_startup(bot) -> None:
     await bot.set_webhook(f"{URL}{PATH}")
 
 
-async def on_shutdown(_):
-    await bot.session.close()
+async def on_shutdown():
+    from database.connect import close_connection
+
+    await close_connection()
 
 
 def main() -> None:  # Запуск бота
+    from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+    from aiohttp import web
+
+    from config import loguru
+    from runners.launch import bot
+    from runners.launch import dp_main as dp
+
+    read_configs()
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
 
@@ -39,10 +50,9 @@ def main() -> None:  # Запуск бота
     )
     webhook_requests_handler.register(app, path=PATH)
 
+    logger.info("Start catching Webhooks")
     setup_application(app, dp, bot=bot)
     web.run_app(app, loop=loop, host=HOST, port=PORT)
-
-    logger.info("Start Webhook")
 
 
 if __name__ == "__main__":

18396b03f54606b2fa5ccb9eed9afd8045bbc00f
Kona,_,Kust <conacust@gmail.com>
2024-02-07 12:10:24 +0700

    ref: Rename admin's menu files

--- bot/admin/handlers/admin_menu.py
+++ bot/admin/handlers/admin_menu.py
@@ -2,7 +2,7 @@ from aiogram import Router, F
 from aiogram.types import CallbackQuery, Message
 from loguru import logger
 from bot.admin.filters.is_admin import IsAdmin
-from bot.admin.keyboards import admin_panel
+from bot.admin.keyboards import admin_menu
 
 router = Router()
 
@@ -11,13 +11,13 @@ router = Router()
 @router.message(F.text.lower() == "/admin", IsAdmin())  # Открыть админку
 async def open_admin_menu_handler(message: Message) -> None:
     await message.delete()
-    await message.answer("*Панель администратора:*", reply_markup=admin_panel.main_kb)
+    await message.answer("*Панель администратора:*", reply_markup=admin_menu.main_kb)
 
 
 @router.callback_query(F.data == "admin_menu", IsAdmin())
 async def to_admin_menu_handler(callback: CallbackQuery) -> None:
     await callback.message.edit_text(
-        "*Панель администратора:*", reply_markup=admin_panel.main_kb
+        "*Панель администратора:*", reply_markup=admin_menu.main_kb
     )
     await callback.answer()
 
@@ -25,7 +25,7 @@ async def to_admin_menu_handler(callback: CallbackQuery) -> None:
 @router.callback_query(F.data == "bot_settings", IsAdmin())
 async def bot_settings_handler(callback: CallbackQuery) -> None:
     await callback.message.edit_text(
-        "Здесь ничего нет, хахах", reply_markup=admin_panel.one_bt
+        "Здесь ничего нет, хахах", reply_markup=admin_menu.one_bt
     )
     await callback.answer()
 
@@ -33,6 +33,6 @@ async def bot_settings_handler(callback: CallbackQuery) -> None:
 @router.callback_query(F.data == "help_for_admin", IsAdmin())
 async def help_for_admin_handler(callback: CallbackQuery) -> None:
     await callback.message.edit_text(
-        "*тебе никто не поможет* ⚰️", reply_markup=admin_panel.sad_kb
+        "*тебе никто не поможет* ⚰️", reply_markup=admin_menu.sad_kb
     )
     await callback.answer()

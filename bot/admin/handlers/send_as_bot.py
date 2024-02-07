572114f086ca6718b11a4ffd11510ff55b2965d5
tftiwnbe <wnbe@hmphin.space>
2024-02-07 12:11:45 +0700

    fix: Supress forbiden error when send as bot

--- bot/admin/handlers/send_as_bot.py
+++ bot/admin/handlers/send_as_bot.py
@@ -1,16 +1,16 @@
-from aiogram import Router, F
-from aiogram.filters import Command, CommandObject
-from aiogram.filters import StateFilter
+from contextlib import suppress
+
+import database.admin as admin_db
+import database.user as user_db
+from aiogram import F, Router
+from aiogram.enums import ParseMode
+from aiogram.exceptions import TelegramForbiddenError
+from aiogram.filters import Command, CommandObject, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.enums import ParseMode
-from aiogram.types import TelegramObject, CallbackQuery
+from aiogram.types import CallbackQuery, TelegramObject
 from bot.admin.filters.is_admin import IsAdmin
-
 from runners.launch import bot
-
-import database.user as user_db
-import database.admin as admin_db
 
 users_db = user_db.Users()
 admin_db = admin_db.Admin()
@@ -36,12 +36,13 @@ async def cmd_notify_all(object: TelegramObject, state: FSMContext) -> None:
 async def send_message_to_all(object: TelegramObject, state: FSMContext) -> None:
     users = await users_db.id_of_users()
     for user in list(users):
-        await bot.copy_message(
-            user,
-            object.from_user.id,
-            object.message_id,
-        )
-        await state.clear()
+        with suppress(TelegramForbiddenError):
+            await bot.copy_message(
+                user,
+                object.from_user.id,
+                object.message_id,
+            )
+    await state.clear()
 
 
 @router.callback_query(F.data == "send_admins", IsAdmin())
@@ -58,9 +59,10 @@ async def cmd_notify_admins(object: TelegramObject, state: FSMContext) -> None:
 async def send_message_to_aadmins(object: TelegramObject, state: FSMContext) -> None:
     admins = await admin_db.id_of_admins()
     for admin in list(admins):
-        await bot.copy_message(
-            admin,
-            object.from_user.id,
-            object.message_id,
-        )
-        await state.clear()
+        with suppress(TelegramForbiddenError):
+            await bot.copy_message(
+                admin,
+                object.from_user.id,
+                object.message_id,
+            )
+    await state.clear()

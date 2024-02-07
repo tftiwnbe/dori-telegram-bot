4f1955093b4cbf92f33aabac80c6cc451618927e
tftiwnbe <wnbe@hmphin.space>
2024-02-07 12:10:24 +0700

    ref: Rename admin's menu files

--- bot/admin/loader.py
+++ bot/admin/loader.py
@@ -1,11 +1,11 @@
 from loguru import logger
-from bot.admin.handlers import admin_panel, user_management, send_as_bot, help_to_users
+from bot.admin.handlers import admin_menu, user_management, send_as_bot, help_to_users
 from runners.launch import dp_main as dp
 
 
 def enable() -> None:  # Регестрация Роутеров
     dp.include_routers(
-        admin_panel.router,
+        admin_menu.router,
         user_management.router,
         send_as_bot.router,
         help_to_users.router,
